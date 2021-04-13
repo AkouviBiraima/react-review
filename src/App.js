b6a29a5cfae4b60a24db821dccebf9bb7d3f9846
AkouviBiraima <josepha.biraima@gmail.com>
2021-04-13 10:33:18 -0400

    handling form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./style.css";
 
 export default function App() {
+  const [person, setPerson] = useState({
+    firstName:'',
+    lastName: '',
+  });
+  const [allFolks, setAllFolks] = useState([])
+
+  function handleInput(name, value){
+    setPerson({...person, [name]: value})
+  }
+
+  function handleSubmit(event){
+    event.preventDefault()
+    setAllFolks([...allFolks], person)
+    setPerson({firstName: "", lastname:""})
+  }
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <form>
+    <input type= "text" name="fname" placeholder="First Name" value={person.firstName} onChange={(event)=>handleInput('firstName', event.target.value)}/>
+    <input type= "text"  name="lname" placeholder="Last Name" value={person.lastName} onChange={(event)=>handleInput('lastName', event.target.value)}/>
+    <input type="submit" onClick={handleSubmit}/>
+    </form>
     </div>
   );
 }
